702316102dc2272d1a1680b67e20589a271aec61
dan13ram <dan13ram@gmail.com>
2022-01-18 14:15:46 +0530

    fix: removed dist folder

--- dist/hardhat.config.d.ts
+++
@@ -1,9 +0,0 @@
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import 'hardhat-gas-reporter';
-import 'solidity-coverage';
-
-import { HardhatUserConfig } from 'hardhat/config';
-declare const config: HardhatUserConfig;
-export default config;
--- dist/test/utils/collabHelpers.d.ts
+++
@@ -1,7 +0,0 @@
-import { TransactionReceipt } from '@ethersproject/abstract-provider';
-export declare const awaitCollabAddress: (
-  receipt: TransactionReceipt,
-) => Promise<any>;
-export declare const TYPES: {
-  [any: string]: string[];
-};
